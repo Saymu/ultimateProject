a763beeb9c72d423db7a19b4adba9e4dcb443591
Saymu <41518761+Saymu@users.noreply.github.com>
2022-08-07 20:53:53 +0200

    project creation

---
+++ src/main/java/com/choukrani/ultimateProject/UltimateProjectApplication.java
@@ -0,0 +1,13 @@
+package com.choukrani.ultimateProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UltimateProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UltimateProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/choukrani/ultimateProject/UltimateProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choukrani.ultimateProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UltimateProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
